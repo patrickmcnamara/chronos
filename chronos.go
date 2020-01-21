07acf4c2264db87df6787a555ffd0d04d58070fc
Patrick McNamara <hello@patrickmcnamara.xyz>
2020-01-21 22:44:54 +0000

    Simplify documentation

--- chronos.go
+++ chronos.go
@@ -25,13 +25,13 @@ const (
 	maxNano int64 = 24 * 60 * 60 * 1000000000
 )
 
-// Now returns the current chronos.
+// Now returns the current time as a Chronos.
 func Now() Chronos {
 	now := time.Now()
 	return FromTime(now)
 }
 
-// FromTime returns a chronos from a time.Time. It ignores the day or date.
+// FromTime returns a Chronos from a time.Time. It ignores the date.
 func FromTime(t time.Time) Chronos {
 	midnight := t.UTC().Truncate(24 * time.Hour)
 	nanoseconds := t.Sub(midnight).Nanoseconds()
@@ -39,25 +39,26 @@ func FromTime(t time.Time) Chronos {
 	return Chronos(moments)
 }
 
-// FromDuration returns a chronos from a time.Duration. Loops over days.
+// FromDuration returns a Chronos from a time.Duration. Loops over days.
 func FromDuration(d time.Duration) Chronos {
 	moments := (d.Nanoseconds() * int64(MaxChronos)) / maxNano
 	return Chronos(moments)
 }
 
-// ToTime creates a time.Time from a chronos. The date will always be the first
+// ToTime creates a time.Time from a Chronos. The date will always be the first
 // day, `0001-01-01`. The timezone will be UTC.
 func (c Chronos) ToTime() time.Time {
 	return time.Date(1, time.January, 1, 0, 0, 0, int(c)*(int(maxNano)/int(MaxChronos)), time.UTC)
 }
 
-// ToDuration creates a time.Duration from a chronos. Loops over days.
+// ToDuration creates a time.Duration from a Chronos. Loops over days.
 func (c Chronos) ToDuration() time.Duration {
 	nanoseconds := int64(c) * (maxNano / int64(MaxChronos))
 	return time.Duration(nanoseconds)
 }
 
-// Parse returns a chronos from a chronos string. The string must be of length 4.
+// Parse returns a Chronos from a chronos string. The string will be truncated
+// to a length of 4.
 func Parse(s string) (c Chronos, err error) {
 	precision := len(s)
 	if precision > 4 {
@@ -67,7 +68,7 @@ func Parse(s string) (c Chronos, err error) {
 	return Chronos(moments), err
 }
 
-// String returns the standard precision chronos string.
+// String returns a string representing the chronos in the form `CCCC`.
 func (c Chronos) String() string {
 	return fmt.Sprintf("%04X", uint16(c))
 }
